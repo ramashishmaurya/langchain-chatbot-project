31bc45cf0c3e341dd0cc8fbec443031458d0c99a
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-23 23:23:38 +0530

    added new file to explore huggingface

---
+++ langchain/ChatModels/opensource.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id= "HuggingFaceH4/zephyr-7b-beta" ,
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat = model.invoke("what is capital of india")
+print(chat.content)
