c66296751a629a48c1cace4d098aecbc5d31697f
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-25 08:10:41 +0530

    use the chain make pipeline easy

--- project.py
+++ project.py
@@ -1,4 +1,7 @@
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+
+templates = PromptTemplate()
 
 llm = ChatOllama(model='llama3')
 
@@ -7,8 +10,15 @@ import streamlit as st
 st.header('Reasearch Tools')
 input_text = st.text_input('Enter the query_here')
 
-if st.button('summarize'):
-    chat = llm.invoke(input_text)
+# this is simple a chain models right 
+if st.button('summarize'): 
+    chain = templates | llm 
+    chat = llm.invoke(input_text) # invoke here
+    # gives the input in this parameters
+    chat2 = chain.invoke({
+        'p1':'person'  , 
+        'p2':'person2'
+    })
     st.write(chat.content)
 
 
